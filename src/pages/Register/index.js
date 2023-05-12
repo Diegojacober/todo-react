ccca7c057f70ae3d3bf86ce103f940f6e53b4379
Diego Alencar Jacober <57761933898>
2023-05-12 15:52:17 -0300

    Pagina de cadastro

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,19 +1,66 @@
 import { useState } from "react"
 import './register.css'
 
+import { auth } from '../../firebaseConnection'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { Link } from "react-router-dom"
 import { toast } from 'react-toastify';
+
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
 
     const [email, setEmail] = useState('')
     const[password, setPassword] = useState('')
+    const navigate = useNavigate()
 
-    function handleRegister(e) {
+    async function handleRegister(e) {
         e.preventDefault()
        
         if(email !== '' && password !== '') {
-           
+           if (password.length >= 6 ) {
+                await createUserWithEmailAndPassword(auth, email, password)
+                .then(() => {
+                    navigate('/admin', { replace: true})
+                })
+                .catch((e) => {
+                    if (e.code == 'auth/email-already-in-use') {
+                        toast.info('Usuário ja cadastrado', {
+                            position: "top-center",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "colored",
+                            });
+                    } else {
+                        toast.error('Erro ao cadastrar usuário, tente novamente mais tarde!', {
+                            position: "top-center",
+                            autoClose: 5000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "colored",
+                            });
+                    }
+                   
+                })
+           } else {
+            toast.error('A senha deve ser maior ou igual a 6 caracteres!', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                });
+           }
         }
          else{
             toast.error('Digite o email e senha para continuar!', {
