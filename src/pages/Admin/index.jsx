5c48cb881e50b152f5c37e301c6fd9f16687dcb9
Diego Alencar Jacober <57761933898>
2023-05-12 16:53:55 -0300

    iniciando tela principal

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,9 +1,38 @@
 import './admin.css'
+import { useState } from 'react'
 
 export default function Admin() {
+
+    const [tarefaInput, setTarefaInput] = useState('')
+
+    async function handleRegister(e) {
+        e.preventDefault()
+
+    }
+
     return(
-        <div>
-            <h1>Admin</h1>
+        <div className='admin-container'>
+           <h1>Minhas tarefas</h1>
+
+           <form onSubmit={handleRegister}>
+
+                <textarea placeholder='Digite sua tarefa...' onChange={(e) => setTarefaInput(e.target.value)}/>
+
+
+                <button type='submit'>Registrar Tarefa</button>
+           </form>
+
+           <article>
+            <p>Estudar js e react</p>
+
+            <div>
+                <button>Editar</button>
+                <button className='btn-delete'>Concluir</button>
+            </div>
+           </article>
+
+
+           <button className='btn-logout'>Sair</button>
         </div>
     )
 }
