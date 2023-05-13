ebaf849d207c8bb38c28d5a1c23185c1c07bbc1c
Diego <diegoalencar.jacober@gmail.ocm>
2023-05-12 22:28:49 -0300

    Configurando redirecionamento de rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,13 @@ import Home from '../pages/Home'
 import Register from '../pages/Register'
 import Admin from '../pages/Admin'
 import Private from './private'
+import Public from './public'
 
 function RoutesApp() {
     return (
         <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/register' element={<Register />} />
+            <Route path='/' element={<Public> <Home /> </Public>} />
+            <Route path='/register' element={<Public> <Register /> </Public>} />
             <Route path='/admin' element={<Private> <Admin /> </Private>} />
             
         </Routes>
